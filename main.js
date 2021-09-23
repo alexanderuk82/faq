b88087210315f73e3bd149a179ed873f724e0bb2
alexanderuk82 <77622127+alexanderuk82@users.noreply.github.com>
2021-09-23 14:32:39 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,8 @@
 'use strict';
 const toggleBtn = document.querySelectorAll('.faq__item--btns');
 
-function remove() {
-  toggleBtn.forEach(btns => {
-    btns.parentNode.classList.remove('active');
-  });
-}
-
 toggleBtn.forEach(btns => {
   btns.addEventListener('click', () => {
-    remove();
     btns.parentNode.classList.toggle('active');
   });
 });
